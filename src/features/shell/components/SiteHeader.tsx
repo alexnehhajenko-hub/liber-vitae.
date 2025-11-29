1048e9904acb76c73be781088c2be5c6f0a75172
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2025-11-29 11:12:32 +0100

    Create SiteHeader.tsx

---
+++ src/features/shell/components/SiteHeader.tsx
@@ -0,0 +1,10 @@
+import { LanguageSwitcher } from "./LanguageSwitcher";
+
+export function SiteHeader() {
+  return (
+    <header>
+      <span>LIBER VITAE</span>
+      <LanguageSwitcher />
+    </header>
+  );
+}
