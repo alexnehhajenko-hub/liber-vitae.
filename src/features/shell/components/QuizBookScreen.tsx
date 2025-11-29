fa0c822ebdec25e45dfd1fcc38096d14953bde27
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2025-11-29 18:34:26 +0100

    Update QuizBookScreen.tsx

--- src/features/shell/components/QuizBookScreen.tsx
+++ src/features/shell/components/QuizBookScreen.tsx
@@ -1,23 +1,6 @@
 import React from "react";
 import { BookLayout } from "./BookLayout";
-import { QuizQuestionPage } from "./QuizQuestionPage";
 
 export function QuizBookScreen() {
-  return (
-    <BookLayout
-      leftPage={<QuizQuestionPage />}
-      rightPage={
-        <div
-          style={{
-            fontSize: 13,
-            lineHeight: 1.5,
-            color: "#5a412e",
-          }}
-        >
-          <strong>Совет.</strong> Отвечайте так, как будто этот человек никогда не прочитает ваши
-          слова. Книга создаётся ради честного взгляда, а не ради комплиментов.
-        </div>
-      }
-    />
-  );
+  return <BookLayout />;
 }
