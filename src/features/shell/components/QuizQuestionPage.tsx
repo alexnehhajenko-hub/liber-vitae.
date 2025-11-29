7aa873a74fa31b805c15541dd0248d6828ed5c4f
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2025-11-29 11:23:15 +0100

    Create QuizQuestionPage.tsx

---
+++ src/features/shell/components/QuizQuestionPage.tsx
@@ -0,0 +1,9 @@
+export function QuizQuestionPage() {
+  return (
+    <div>
+      <h2>Вопрос</h2>
+      <p>Здесь будет текст вопроса.</p>
+      <textarea placeholder="Ваш ответ..." />
+    </div>
+  );
+}
