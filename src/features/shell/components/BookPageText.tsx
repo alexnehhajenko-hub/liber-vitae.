686d9c5374c50295334a75ddec592301c3183726
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2025-11-29 11:44:31 +0100

    Update BookPageText.tsx

--- src/features/shell/components/BookPageText.tsx
+++ src/features/shell/components/BookPageText.tsx
@@ -1,3 +1,19 @@
-export function BookPageText() {
-  return <div>LIBER VITAE: [Имя]. Здесь будет текст книги.</div>;
+import React from "react";
+
+type BookPageTextProps = {
+  name: string;
+  text: string;
+};
+
+export function BookPageText({ name, text }: BookPageTextProps) {
+  return (
+    <div>
+      <div className="lv-book-heading">LIBER VITAE</div>
+      <div className="lv-book-subheading">{name.toUpperCase()}</div>
+      <div className="lv-book-body">
+        <span className="lv-dropcap">{text.charAt(0)}</span>
+        {text.slice(1)}
+      </div>
+    </div>
+  );
 }
