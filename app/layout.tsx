c5c912c28bc68d1898228983904a3db476ab7dfe
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2025-11-29 12:08:14 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+import { SiteLayout } from "../src/features/shell/components/SiteLayout";
+import { HeroSection } from "../src/features/shell/components/HeroSection";
+
+export default function HomePage() {
+  return (
+    <SiteLayout>
+      <HeroSection />
+    </SiteLayout>
+  );
+}
