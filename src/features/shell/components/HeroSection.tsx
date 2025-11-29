7209ff7d1d590b26cf7b30fc4f24d8bd87719e9b
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2025-11-29 11:17:54 +0100

    Create HeroSection.tsx

---
+++ src/features/shell/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../../../shared/ui/Button";
+
+export function HeroSection() {
+  return (
+    <section>
+      <h1>Философская книга о человеке</h1>
+      <p>Книга и портрет в стиле старинного манускрипта.</p>
+      <Button>Книга обо мне</Button>
+      <Button>Книга о близком человеке</Button>
+    </section>
+  );
+}
