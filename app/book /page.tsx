703d517b70d848aeb2fdeddbaf405ca29783c38a
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2025-12-02 15:35:04 +0100

    Update page.tsx

--- app/book /page.tsx
+++ app/book /page.tsx
@@ -1,6 +1,6 @@
 // app/book/page.tsx
-import React from "react";
-import { BookLayout } from "../../src/features/shell/components/BookLayout";
+
+import BookLayout from "../../src/features/shell/components/BookLayout";
 
 export default function BookPage() {
   return <BookLayout />;
