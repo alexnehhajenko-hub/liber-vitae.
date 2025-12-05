c53af4a4fb385889303eabf370e83cb0113c497f
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2025-12-05 11:48:56 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,12 @@
 // app/page.tsx
 import React from "react";
 import { SiteLayout } from "@/features/shell/components/SiteLayout";
-import { BookLayout } from "@/features/shell/components/BookLayout";
+import { HeroSection } from "@/features/shell/components/HeroSection";
 
 export default function HomePage() {
   return (
     <SiteLayout>
-      {/* Контейнер книги:
-          - поднимаем книгу вверх ~на 20 мм
-          - блокируем горизонтальный свайп браузера (чтобы не улетать со страницы) */}
-      <div
-        style={{
-          marginTop: "-20mm",          // тянем выше
-          touchAction: "pan-y",        // браузеру можно только вертикальный скролл
-          overscrollBehaviorX: "none", // не срабатывать жест "назад/вперёд" по краям экрана
-        }}
-      >
-        <BookLayout />
-      </div>
+      <HeroSection />
     </SiteLayout>
   );
 }
