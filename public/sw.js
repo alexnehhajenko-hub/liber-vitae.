b03e42b387a062cc2a5a28b907e367d6b4d2edb4
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2025-12-22 19:04:18 +0000

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'lv-cache-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/book',
+        '/manifest.webmanifest'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
